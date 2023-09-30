7e7d883aad3cb97b85c8d157e6c6eed4a151abe0
IwanovAS <iwanov.if.ua@gmail.com>
2023-09-30 21:33:51 +0300

    all good

---
+++ src/components/App/App.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { FeedbackOptions } from 'components/FeedBackOptions/FeedbackOptions';
+import { Section } from 'components/Section/Section';
+import { Statistics } from 'components/Statistics/Statistics';
+import { Notification } from 'components/Notification/Notification';
+import css from './App.module.css'
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = e => {
+    if (e === 'Good') {
+      this.setState({ good: this.state.good + 1 });
+    } else if (e === 'Neutral') {
+      this.setState({ neutral: this.state.neutral + 1 });
+    } else if (e === 'Bad') {
+      this.setState({ bad: this.state.bad + 1 });
+    }
+  };
+
+  totalFeedback = () => {
+    let total = this.state.good + this.state.neutral + this.state.bad;
+    return total;
+  };
+
+  positivePercentage = () => {
+    if (this.totalFeedback() === 0) {
+      return 0;
+    }
+    return Math.round((this.state.good / this.totalFeedback()) * 100);
+  };
+
+  render() {
+    return (
+      <div className={css.container}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['Good', 'Neutral', 'Bad']}
+            onLeaveFeedback={this.handleFeedback}
+          />{' '}
+        </Section>
+
+        <Section title="Statistics">
+          {this.totalFeedback() !== 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.totalFeedback()}
+              positivePercentage={this.positivePercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
